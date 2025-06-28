541253b4472ee152d33564226a80b58d01ed5328
Akshay Asok <akshayasokson@gmail.com>
2025-06-28 20:23:51 +0530

    app completed

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,71 @@
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [invalid, setInvalid] = useState(false);
+  const [login, setLogin] = useState(false);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log("submit >> ", e.target.elements);
+
+    if(username === "user" && password === "password"){
+      setLogin(true);
+    }
+    else {
+      setInvalid(true);
+    }
+  }
+
   return (
     <div className="App">
-      XLogin
+      
+        {
+          login ? (<p>Welcome User</p>) : (
+                <>
+                <h1>Login Page</h1>
+
+                  {
+                    invalid && <p>Invalid username or password</p>
+                  }
+
+              <form name='loginForm'
+              onSubmit={handleSubmit}
+              >
+
+                <label for="username" >Username:</label>
+                <input id='username' placeholder='Username' required type='text' 
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+                />
+
+                <br />
+
+                <label for="password">Password:</label>
+                <input id='password' placeholder='Password' required type='text' 
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value)
+                }}
+                />
+
+                <br />
+
+                <button
+                type='submit'
+                >Submit</button>
+              </form>
+              </>
+          )
+        }
+
     </div>
   );
 }
